71cdad779cd781843618cbe0328df30d664fe068
rakshithv <rak.raju@gmail.com>
2018-09-13 20:24:13 -0500

    Add doc strings

--- gedcom_types.py
+++ gedcom_types.py
@@ -10,6 +10,10 @@ from collections import namedtuple
 Validation_Results = namedtuple("Validation_Results", ['valid', 'level', 'tag', 'args'])
 
 class Individual:
+    '''This is a datastructure that holds information about an individual.
+    It currently saves data about the individual identifier, individual name,
+    date of birth, date of death, family id of any children, and
+    family id of any spouse'''
     
     # Map GEDCOM tag into instance fields (currently 1:1)
     TAG_MAP = {
@@ -30,13 +34,13 @@ class Individual:
         self.child_family_ids = []
         self.spouse_family_ids = []
         
-    def __str__(self):
-        return self.id + " name=" + self.name + " dob=" + str(self.birth) + \
-        " child-of=" + str(self.child_family_ids) + " spouse-of=" + str(self.spouse_family_ids)
-     
     # Right now, just push values as strings straight into the fields.
     # Parsing into dates/etc can come later.
     def parse_value(self, gedcomTag,  gedcomValue):
+        '''This function determines which fields to add into the individual
+        record. If it is a child or spouse record, it will be added to the 
+        ids list. Otherwise the remaining tags will be added as the 
+        individual record attributes'''
         if gedcomTag == "FAMC":
             self.child_family_ids.append(gedcomValue)
         elif gedcomTag == "FAMS":
@@ -45,6 +49,9 @@ class Individual:
             setattr(self, Individual.TAG_MAP[gedcomTag], gedcomValue)
 
 class Family:
+    '''This is a datastructure to store information about a family.
+    The datastructure currently holds values of family id, date of 
+    marriage, date of divorce, husband id, wife id, and children ids'''
     
     # Map GEDCOM tag into instance fields (currently 1:1)
     TAG_MAP = {
@@ -62,12 +69,12 @@ class Family:
         self.wife_id = None
         self.children_id_list = []
         
-    def __str__(self):
-        return self.id + " husb=" + str(self.husband_id) + " wife=" + str(self.wife_id) + " children=" + str(self.children_id_list)
-    
     # Right now, just push values as strings straight into the fields.
     # Parsing into dates/etc can come later.
     def parse_value(self, gedcomTag,  gedcomValue):
+        '''This function determines which fields to add to the family
+        record. If it encounters children ids, it adds it to the children
+        list. Otherwise, if adds it as new data to the family record'''
         if gedcomTag == "CHIL":
             self.children_id_list.append(gedcomValue)
         else:
