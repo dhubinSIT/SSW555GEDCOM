c3b02e433cc493281696aef640ebd364de3398fd
aperry567 <36387436+aperry567@users.noreply.github.com>
2018-09-25 21:37:07 -0400

    Revised unittest code for user story 29
    US29

--- tests/test_us29.py
+++ tests/test_us29.py
@@ -1,10 +1,20 @@
 import sys
 import unittest
-from gedcom_parser import parse_file
-from gedcom_types import Family,Individual
+from gedcom import printDeceased
+from gedcom_parser import parse_file,  parse_date
+from gedcom_validation import collect_validation_warnings
+from gedcom_types import Family,  Individual
 
-
-        
+#US29 - List of Deceased
+class printDeceasedTest(unittest.TestCase):     
+    def test_printDeceased(self):
+        """List of Deceased."""
+        self.assertTrue("printDeceased", parse_file("15 AUG 1999"))
+        self.assertNotIn("printDeceased",parse_file("29 JUN 2005"))
+        self.assertTrue("printDeceased",parse_file("11 Nov 1995"))
+        self.assertNotEqual("printDeceased",parse_file("13 Nov 1995"))
+        self.assertNotIn("printDeceased",parse_file("7 MAY 2009"))   
+   
 #UnitTest function 
 def main():
     '''main() function'''
