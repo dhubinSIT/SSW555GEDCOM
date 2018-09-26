2a066dec20ef1350948e6131e33b6d9c1e3fd7b2
aperry567 <36387436+aperry567@users.noreply.github.com>
2018-09-25 21:35:57 -0400

    Revised unittest for user story 28
    US28 completed

--- tests/test_us28.py
+++ tests/test_us28.py
@@ -1,10 +1,17 @@
 import sys
 import unittest
-from gedcom_parser import parse_file
-from gedcom_types import Family,Individual
+from gedcom import printSiblings
+from gedcom_parser import parse_file,  parse_date
+from gedcom_validation import collect_validation_warnings
+from gedcom_types import Family,  Individual
 
-
-        
+#US28 - Order Siblings by Age
+class printSiblingsTest(unittest.TestCase):     
+    def test_printSiblings(self):
+        """Order Siblings by Age."""
+        self.assertTrue("printSiblings", parse_file("45"))
+        self.assertNotIn("printSiblings",parse_file("89"))
+   
 #UnitTest function 
 def main():
     '''main() function'''
