4fad48679239fc8685fde1812adaaaba1ddfcdb7
rakshithv <rak.raju@gmail.com>
2018-09-26 19:59:01 -0500

    Commit updates for US02
    Commit changes for checking if Birth Date is after Marriage date. Commit test file Story US02

--- gedcom_validation.py
+++ gedcom_validation.py
@@ -3,6 +3,7 @@
 # David Hubin
 # Ayana Perry
 # Rakshith Varadaraju
+from gedcom_types import Validation_Results
 
 def collect_validation_warnings(individuals,  families):
     """This procedure will run through every validation routine
@@ -13,5 +14,16 @@ def collect_validation_warnings(individuals,  families):
         warnings = warnings + i.validate()
     for f in families.values():
         warnings = warnings + f.validate()
+
+    warnings = warnings + _US02_Birth_Before_Marriage(individuals, families)
     return warnings
 
+def _US02_Birth_Before_Marriage(individuals, families):
+    warnings = []
+    for fam in families:
+        if families[fam].married != None:
+            if families[fam].husband_id != None and individuals[families[fam].husband_id].birth != None and individuals[families[fam].husband_id].birth > families[fam].married:
+                warnings.append(Validation_Results("US02", 'Individual %s has a birth date after marriage date.' % families[fam].husband_id))
+            if families[fam].wife_id != None and individuals[families[fam].wife_id].birth != None and individuals[families[fam].wife_id].birth > families[fam].married:
+                warnings.append(Validation_Results("US02", 'Individual %s has a birth date after marriage date.' % families[fam].wife_id))        
+    return warnings
---
+++ tests/test_us02.py
@@ -0,0 +1,100 @@
+# SSW 555 Project GEDCOM Parser / Validator
+#
+# David Hubin
+# Ayana Perry
+# Rakshith Varadaraju
+
+from io import StringIO
+import unittest
+from gedcom_types import Individual, Family
+from gedcom_parser import parse_date, parse_file
+from gedcom_validation import collect_validation_warnings, _US02_Birth_Before_Marriage
+
+class US03TestCase(unittest.TestCase):
+    def shortDescription(self):
+        '''Disable printing docstring on verbose'''
+        return None
+
+    def setUp(self):
+        self.OkayIndi = Individual('@okay@')
+        self.OkayFam = Family('@okayf@')
+        self.OkayIndi.apply_value('BIRT', parse_date('1 JUN 1945'))
+        self.OkayIndi.apply_value('DEAT', parse_date('6 JAN 1995'))
+        self.OkayFam.apply_value('MARR', parse_date('1 JUN 1975'))
+        self.OkayFam.apply_value('HUSB', '@okay@')
+
+        self.BadBirth = Individual('@birth@')
+        self.BadFam = Family('@badf@')
+        self.BadBirth.apply_value('BIRT', parse_date('7 JAN 1995'))
+        self.BadBirth.apply_value('DEAT', parse_date('6 JAN 1995'))
+        self.BadFam.apply_value('MARR', parse_date('6 JAN 1995'))
+        self.BadFam.apply_value('HUSB', '@birth@')
+
+        self.MissingBirth = Individual('@nobirth@')
+        self.MissingBirthFam = Family('@badfnb@')
+        self.MissingBirth.apply_value('DEAT', parse_date('6 JAN 1995'))
+        self.MissingBirthFam.apply_value('MARR', parse_date('6 JAN 1995'))
+        self.MissingBirthFam.apply_value('HUSB', '@nobirth@')
+
+        self.MissingMarr = Individual('@nomarr@')
+        self.MissingMarrFam = Family('@badfnm@')
+        self.MissingMarr.apply_value('BIRT', parse_date('7 JAN 1995'))
+        self.MissingMarr.apply_value('DEAT', parse_date('6 JAN 1995'))
+        self.MissingMarrFam.apply_value('HUSB', '@nomarr@')
+		
+		
+    def test_birth_before_marr(self):
+        '''Test warnings if date of birth is after date of marriage'''
+        warnings = _US02_Birth_Before_Marriage({'@okay@' : self.OkayIndi, '@birth@' : self.BadBirth},
+                                               {'@okayf@' : self.OkayFam, '@badf@' : self.BadFam})
+        self.assertEqual(len(warnings), 1)
+        self.assertTrue('@birth@' in warnings[0].message and 'birth date after marriage date' in warnings[0].message)
+
+    def test_no_birth(self):
+        '''Test warnings if no date of birth'''
+        warnings = _US02_Birth_Before_Marriage({'@okay@' : self.OkayIndi, '@nobirth@' : self.MissingBirth},
+                                               {'@okayf@' : self.OkayFam, '@badfnb@' : self.MissingBirthFam})
+        self.assertEqual(len(warnings), 0)
+        
+    def test_no_marriage(self):
+        '''Test warnings if no date of marriage'''
+        warnings = _US02_Birth_Before_Marriage({'@okay@' : self.OkayIndi, '@nomarr@' : self.MissingMarr},
+                                               {'@okayf@' : self.OkayFam, '@badfnm@' : self.MissingMarrFam})
+        self.assertEqual(len(warnings), 0)
+        
+		
+    def test_okay_individual(self):
+        '''Test warnings if date of birth is after date of death'''
+        warnings = _US02_Birth_Before_Marriage({'@okay@' : self.OkayIndi},
+                                               {'@okayf@' : self.OkayFam})
+        self.assertEqual(len(warnings), 0)
+
+    def test_full_path(self):
+        '''Integration test for parser->objects->validation'''
+        buff = StringIO('''0 @I1@ INDI
+1 NAME CrankyFrank /Coffin/
+1 SEX M
+1 BIRT
+2 DATE 3 MAR 1986
+1 DEAT
+2 DATE 3 MAR 1986
+1 FAMS @F1@
+0 @I2@ INDI
+1 NAME SomethingSally /Coffin/
+1 SEX F
+1 BIRT
+2 DATE 2 MAR 1986
+1 DEAT
+2 DATE 2 MAR 1986
+1 FAMS @F1@
+0 @F1@ FAM
+1 HUSB @I1@
+1 WIFE @I2@
+1 MARR
+2 DATE 2 MAR 1986''')
+        (ind, fam, unused_warns) = parse_file(buff)
+        warnings = collect_validation_warnings(ind, fam)
+        count_of_us02_errors = 0
+        for warn in warnings:
+            if warn.story == "US02": count_of_us02_errors += 1
+        self.assertEqual(count_of_us02_errors, 1)
