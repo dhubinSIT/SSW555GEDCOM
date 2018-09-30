488910c957da8a02d21e582fc08ddfa306568848
dhubinSIT <dhubin@stevens.edu>
2018-09-30 07:23:13 -0500

    Test missing references and the valid case.

--- tests/test_us26.py
+++ tests/test_us26.py
@@ -9,7 +9,7 @@ import unittest
 from gedcom_parser import parse_file
 from gedcom_validation import collect_validation_warnings
 
-class US01TestCase(unittest.TestCase):
+class US26TestCase(unittest.TestCase):
     def shortDescription(self):
         """Disable printing docstring on verbose."""
         return None
@@ -54,4 +54,37 @@ class US01TestCase(unittest.TestCase):
         ind2_warns = ind['@I2@']._Check_References()
         self.assertTrue(len([x for x in ind2_warns if x.story == "US26"]) == 1) # ind2 isn't a child of fam1
         self.assertTrue(len([x for x in ind2_warns if "@F1@" in x.message and "child" in x.message]) == 1) # ind2 isn't a spouse of fam1
+        
+    def test_missing_references(self):
+        """Test references which are missing altogether. 
+        These are just the parser warnings for missing individuals."""
+        buff = StringIO("""0 @I1@ INDI
+1 FAMS @FmissingS@
+1 FAMC @FmissingC@
+0 @F1@ FAM
+1 WIFE @ImissingW@
+1 HUSB @ImissingH@
+1 CHIL @ImissingC@""")
+        (ind,  fam,  warns) = parse_file(buff)
+        self.assertTrue(len([x for x in warns if x.story == "US26"]) == 5)
+        
+    def test_everything_fine(self):
+        """Test that an okay GEDCOM file is reported with no warnings."""
+        buff = StringIO("""0 @I1@ INDI
+1 FAMC @F1@
+0 @I2@ INDI
+1 FAMC @F1@
+0 @I3@ INDI
+1 FAMS @F1@
+0 @I4@ INDI
+1 FAMS @F1@
+0 @F1@ FAM
+1 HUSB @I3@
+1 WIFE @I4@
+1 CHIL @I1@
+1 CHIL @I2@""")
+        (ind,  fam,  warns) = parse_file(buff)
+        validation_warns = collect_validation_warnings(ind,  fam)
+        us26_warns = [x for x in warns + validation_warns if x.story == "US26"]
+        self.assertTrue(len(us26_warns) == 0)
         
