54c354174e7d6ef1e1957a006f880ea9251ec98a
aperry567 <36387436+aperry567@users.noreply.github.com>
2018-10-01 19:52:02 -0400

    Final code for unit test US31

--- tests/test_us31.py
+++ tests/test_us31.py
@@ -12,13 +12,14 @@ from gedcom_parser import parse_date
 class US31TestCase(unittest.TestCase):
     def setUp(self):
         self.OkayIndi1 = Individual('@okay1@')
-        self.OkayIndi1.apply_value('DEAT', parse_date('1 JUN 1945'))
+        self.OkayIndi1.apply_value('BIRT', parse_date('1 JUN 1985'))
+        self.OkayIndi1.apply_value('DEAT', parse_date('1 JUN 1985'))
         self.OkayIndi1.apply_value('NAME', 'Prince William')
 
     def test_US31_Listofliving(self):
-        """ List of Living"""
-        self.assertFalse(US31_Listofliving({'@okay1@': self.OkayIndi1}) != 'Prince William')
-        self.assertTrue(US31_Listofliving({'@okay1@': self.OkayIndi1}) == 'Prince William')
+        """List of Living."""
+        self.assertFalse(US31_Listofliving({'@okay1@': self.OkayIndi1}) == 'Prince William')
+        self.assertTrue(US31_Listofliving({'@okay1@': self.OkayIndi1}) != 'Prince William')
 
 #UnitTest function 
 def main():
@@ -26,5 +27,4 @@ def main():
           
 if __name__ == "__main__":
     unittest.main(exit=False, verbosity=2)
-    main()
-© 2018 GitHub, Inc.
+    main() 
