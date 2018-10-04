116d1571d567a79fb9605d39b6a7a2c0001c4176
aperry567 <36387436+aperry567@users.noreply.github.com>
2018-10-03 20:54:04 -0400

    Latest team gedcom file as of 10/3/2018
    Refactored US29

--- gedcom.py
+++ gedcom.py
@@ -12,6 +12,7 @@ from prettytable import PrettyTable
 from gedcom_parser import parse_file
 import gedcom_validation
 
+
 def datestring(d):
     """Pretty-print a date for output."""
     if d != None:
@@ -19,29 +20,38 @@ def datestring(d):
     else:
         return ""
 
-def lookupname(ref):
-    """Returns the string representing value, or a blank string if None"""
-    return (ref.name if (ref != None) else "")
 
-def printIndividuals(individuals):
+def lookupname(individuals, key):
+    if key == None:
+        return ""
+    else:
+        return individuals[key].name
+
+
+def printIndividuals(indi):
     '''Produce and print the table of individuals.'''
     print('Individuals')
-    pt = PrettyTable(field_names=['ID','Name','Gender','Birthday','Death','Child','Spouse'])
-    for ind in sorted(individuals.values(),  key=lambda x: x.id):
-        pt.add_row([ind.id,ind.name,ind.gender,datestring(ind.birth),datestring(ind.death),ind.child_family_ids,ind.spouse_family_ids])
+    pt = PrettyTable(field_names=['ID', 'Name', 'Gender', 'Birthday', 'Death', 'Child', 'Spouse'])
+    for x in sorted(indi):
+        pt.add_row([indi[x].id, indi[x].name, indi[x].gender, datestring(indi[x].birth), datestring(indi[x].death),
+                    indi[x].child_family_ids, indi[x].spouse_family_ids])
     print(pt)
 
-def printFamilies(individuals, families):
+
+def printFamilies(indi, fam):
     '''Produce and print the table of families.'''
     print('Families')
-    pt = PrettyTable(field_names=['ID','Married','Divorced','Husband ID','Husband Name','Wife ID','Wife Name','Children'])
-    for fam in sorted(families.values(),  key=lambda x: x.id):
-        husband_name = lookupname(fam.husband)
-        wife_name = lookupname(fam.wife)
-        pt.add_row([fam.id,datestring(fam.married),datestring(fam.divorced),fam.husband_id,husband_name,fam.wife_id,wife_name,fam.children_id_list])
+    pt = PrettyTable(
+        field_names=['ID', 'Married', 'Divorced', 'Husband ID', 'Husband Name', 'Wife ID', 'Wife Name', 'Children'])
+    for x in sorted(fam):
+        husband_name = lookupname(indi, fam[x].husband_id)
+        wife_name = lookupname(indi, fam[x].wife_id)
+        pt.add_row([fam[x].id, datestring(fam[x].married), datestring(fam[x].divorced), fam[x].husband_id, husband_name,
+                    fam[x].wife_id, wife_name, fam[x].children_id_list])
     print(pt)
-    
-def US28_SiblingsByAge(siblingslist,indi, fam):
+
+
+def US28_SiblingsByAge(siblingslist, indi, fam):
     '''Refactor for unittest '''
     now = datetime.date.today()
     siblings_age = dict()
@@ -52,51 +62,55 @@ def US28_SiblingsByAge(siblingslist,indi, fam):
         siblings_age[names] = integeryears
     return siblings_age
 
-def US28_SiblingsByAge_pt(indi,fam):
+
+def US28_SiblingsByAge_pt(indi, fam):
     '''A's User story 28-Order Siblings by Age.'''
     print('US28_Siblings by their ages')
     for x in sorted(fam):
-        pt = PrettyTable(field_names=['Siblings_Names','Siblings_Ages'])
+        pt = PrettyTable(field_names=['Siblings_Names', 'Siblings_Ages'])
         siblings_age = dict()
         siblingslist = fam[x].children_id_list
-        siblings_age = US28_SiblingsByAge(siblingslist,indi,fam)
-        for y in sorted(siblings_age, key=lambda y: siblings_age[y], reverse=True): 
+        siblings_age = US28_SiblingsByAge(siblingslist, indi, fam)
+        for y in sorted(siblings_age, key=lambda y: siblings_age[y], reverse=True):
             age = siblings_age[y]
             pt.add_row([y, age])
         print(pt)
 
+
 def US29_ListOfDeceased(indi):
     '''AP's User story 29 - List Deceased.'''
-    print('US29_List of Deceased')
-    pt = PrettyTable(field_names=['Deceased_Names','Deceased_Date'])
-    for x in (indi):
+    pt = PrettyTable(field_names=['Deceased_Names', 'Deceased_Date'])
+    print('List of Deceased')
+    for x in sorted(indi):
         deceased = list()
         if indi[x].death != None:
             died = datestring(indi[x].death)
             names = indi[x].name
             deceased.append(names)
-            deceased.append(died)
-            pt.add_row(deceased)
+            pt.add_row([names,died])
     print(pt)
-    return names
+    return deceased
 
-def US30_ListLivingMarried_pt(indi,fam):
-    '''AP's User story 30 - List living married'''  
+
+def US30_ListLivingMarried_pt(indi, fam):
+    '''AP's User story 30 - List living married'''
     print('List living married')
     pt = PrettyTable(field_names=['Husband', 'Wife'])
-    marriedFamilies = US30_Listlivingmarried_main(indi,fam)
+    marriedFamilies = US30_Listlivingmarried_main(indi, fam)
     for family in marriedFamilies:
         pt.add_row([indi[family.husband_id].name, indi[family.wife_id].name])
-    print(pt)    
+    print(pt)
 
-def US30_Listlivingmarried_main(indi,fam):
+
+def US30_Listlivingmarried_main(indi, fam):
     '''AP's User story 30 - List living married'''
     married = list()
     for f in fam:
         if fam[f].divorced == None and fam[f].husband_id != None and indi[fam[f].husband_id].death == None and \
-           fam[f].wife_id != None and indi[fam[f].wife_id].death == None:
+                fam[f].wife_id != None and indi[fam[f].wife_id].death == None:
             married.append(fam[f])
     return married
+
 
 def US31_Listofliving(indi):
     """ AP User story 31 - List of Living """
@@ -119,8 +133,7 @@ def printWarnings(warnings):
         print('Warnings')
         pt = PrettyTable(field_names=['Code', 'Message'])
         for warn in warnings:
-            pt.add_row([warn.story,  warn.message])
-        pt.sortby = "Code"
+            pt.add_row([warn.story, warn.message])
         print(pt)
     else:
         print()
@@ -129,15 +142,14 @@ def printWarnings(warnings):
 
 if __name__ == "__main__":
     '''Begin by opening the file '''
-    with open (sys.argv[1], 'r') as f:
-        (indi, fam,  parse_warns) = parse_file(f)
-        warnings = parse_warns + gedcom_validation.collect_validation_warnings(indi,  fam)
-        
+    with open('C:/Users/Ayana Perry/Documents/StevensInstituteofTechnology/Hubin_fictional_family.ged', 'r') as f:
+        (indi, fam, parse_warns) = parse_file(f)
+        warnings = parse_warns + gedcom_validation.collect_validation_warnings(indi, fam)
+
         printIndividuals(indi)
         printFamilies(indi, fam)
         US28_SiblingsByAge_pt(indi, fam)
         US29_ListOfDeceased(indi)
-        US30_ListLivingMarried_pt(indi,fam)
+        US30_ListLivingMarried_pt(indi, fam)
         US31_Listofliving(indi)
         printWarnings(warnings)
-
