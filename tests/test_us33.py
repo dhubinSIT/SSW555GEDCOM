676db78541051754b094acaa8475549c474e91d0
aperry567 <36387436+aperry567@users.noreply.github.com>
2018-10-15 21:47:31 -0400

    Updated test_us33.py file

--- tests/test_us33.py
+++ tests/test_us33.py
@@ -1,4 +1,3 @@
-
 # SSW 555 Project GEDCOM Parser / Validator
 #
 # David Hubin
@@ -10,29 +9,32 @@ from gedcom import *
 from gedcom_types import *
 from gedcom_parser import parse_date
 
+
 class US33TestCase(unittest.TestCase):
     def setUp(self):
         self.OkayIndi1 = Individual('@okay1@')
         self.OkayIndi2 = Individual('@okay2@')
-        self.OkayIndi1.apply_value('BIRT', parse_date('1 JUN 1945'))
-        self.OkayIndi2.apply_value('BIRT', parse_date('1 JUN 2011'))
-        self.OkayIndi1.apply_value('NAME', 'Prince William')
-        self.OkayIndi2.apply_value('NAME', 'Princess Kate')
+        self.OkayIndi1.apply_value('NAME', 'Lucy /Simmons/')
+        self.OkayIndi2.apply_value('NAME', 'Patrick /Simmons/')
         self.OkayFam = Family('@okayf@')
         self.OkayFam.apply_value('CHIL', '@okay1@')
+        self.OkayFam.apply_value('CHIL', '@okay2@')
 
     def test_US33_ListOrphans_main(self):
-        """List of Orphans."""
-        ListofOrphans = US33_ListOrphans_main({'@okay1@': self.OkayIndi1,'@okay2@': self.OkayIndi2}, {'@okayf@': self.OkayFam})
-        self.assertTrue(self.OkayFam in ListofOrphans)
+        """List of Orphans"""
+        Listoforphans = US33_ListOrphans_main({'@okay1@': self.OkayIndi1, '@okay2@': self.OkayIndi2}, \
+                                              {'@okayf@': self.OkayFam})
+        self.assertFalse(self.OkayFam in Listoforphans)
+        self.assertFalse(self.OkayIndi2 in Listoforphans)
 
-#UnitTest function 
+
+# UnitTest function
 def main():
     '''main() function'''
-          
+
+
 if __name__ == "__main__":
     unittest.main(exit=False, verbosity=2)
-    main() 
-
-    
+    main()
+ 
     
