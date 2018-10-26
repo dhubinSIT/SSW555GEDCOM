23b3ab402b3e339190de6240747c641c65a6a18f
dhubinSIT <dhubin@stevens.edu>
2018-10-25 19:35:53 -0500

    Sprint 4 US23 Unique name and birth date

--- gedcom_validation.py
+++ gedcom_validation.py
@@ -15,7 +15,10 @@ def collect_validation_warnings(individuals,  families):
     for f in families.values():
         warnings = warnings + f.validate()
 
-    warnings = warnings + _US02_Birth_Before_Marriage(individuals, families) + _US05_Death_Before_Marriage(individuals, families)
+    warnings = warnings +\
+               _US02_Birth_Before_Marriage(individuals, families) +\
+               _US05_Death_Before_Marriage(individuals, families) +\
+               _US23_Unique_Individuals(individuals)
     return warnings
 
 def _US02_Birth_Before_Marriage(individuals, families):
@@ -36,4 +39,19 @@ def _US05_Death_Before_Marriage(individuals, families):
                 warnings.append(Validation_Results("US05", 'Individual %s has a death date before marriage date.' % families[fam].husband_id))
             if families[fam].wife_id != None and families[fam].wife.death != None and families[fam].wife.death < families[fam].married:
                 warnings.append(Validation_Results("US05", 'Individual %s has a death date before marriage date.' % families[fam].wife_id))        
+    return warnings
+
+def _US23_Unique_Individuals(individuals):
+    """US23: Unique name and birth date.  Only covering those individuals that have both fields defined."""
+    reverse_individuals = {}
+    
+    for individual in filter(lambda x: x.birth != None and x.name != None, individuals.values()):
+        key = individual.name + str(individual.birth)
+        if key not in reverse_individuals:
+            reverse_individuals[key] = [individual.id]
+        else:
+            reverse_individuals[key].append(individual.id)
+            
+    warnings = list(map(lambda group: Validation_Results("US23", "The following individuals all have the same name and birthdate: %s" % group), 
+                        filter(lambda group: len(group) > 1,  reverse_individuals.values())))
     return warnings
